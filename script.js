549c8c8db1e5c9463df8cc0a90c080c3e6857051
securityfb <wickanderson326@gmail.com>
2025-04-07 12:11:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function callMe(){
-alert("calllllllllllllll");
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify({
+  "name": "john doe",
+  "phone": "12345"
+});
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch("https://userapi-vzog.onrender.com/users", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
 }
 
 callMe();
